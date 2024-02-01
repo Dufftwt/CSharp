f9e382a04ace5562813525b0d328acca8e8be391
Duff <dilan3268@gmail.com>
2024-02-01 14:13:44 +0100

    typo

--- exo1/Base1/Program.cs
+++ exo1/Base1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Poser une question à l'utilisateur
-            Console.WriteLine("Quel est votre nom ?");
+Console.WriteLine("Quel est votre nom ?");
 
-            // Récupérer la réponse de l'utilisateur
-            string nomUtilisateur = Console.ReadLine();
+// Récupérer la réponse de l'utilisateur
+string nomUtilisateur = Console.ReadLine();
 
-            // Afficher la question suivie de la réponse
-            Console.WriteLine("Votre nom est : " + nomUtilisateur);
+// Afficher la question suivie de la réponse
+Console.WriteLine("Votre nom est : " + nomUtilisateur);
