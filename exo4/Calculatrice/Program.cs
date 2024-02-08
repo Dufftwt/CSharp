9d10927665fc7b45c35d3ce2a9a046192dedcc00
Duff <dilan3268@gmail.com>
2024-02-08 14:11:45 +0100

    ok

---
+++ exo4/Calculatrice/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Calculatrice en C# (METTRE DES ESPACES ENTRE LES CHIFFRES ET LES OPÉRATEURS)");
+Console.WriteLine("Entrez un simple calcul (ex: 5 + 3): ");
+
+string input = Console.ReadLine(); 
+string[] parts = input.Split(' '); 
+
+double a = double.Parse(parts[0]); 
+double b = double.Parse(parts[2]); 
+
+double result = 0;
+
+switch (parts[1]) 
+{
+		case "+":
+				result = a + b;
+				break;
+		case "-":
+				result = a - b;
+				break;
+		case "*":
+				result = a * b;
+				break;
+		case "/":
+				result = a / b;
+				break;
+		default:
+				Console.WriteLine("Opérateur inconnu");
+				return;
+}
+
+Console.WriteLine("Résultat: " + result);
