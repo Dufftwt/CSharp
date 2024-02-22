adb3647118c30c9dd872fd73878d4076a9570e9b
Duff <dilan3268@gmail.com>
2024-02-22 09:46:44 +0100

    ajouter multiplication

---
+++ multiplication/multiplication/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Donnez un nombre pour en afficher la table de multiplication: ");
+if (!int.TryParse(Console.ReadLine(), out int number))
+{
+		Console.WriteLine("ERREUR: ce n'est pas un nombre entier ou le nombre est trop gros");
+		return;
+}
+
+for (int i = 1; i <= 11; i++)
+{
+		Console.WriteLine(number + " * " + i + " = " + number * i);
+}
